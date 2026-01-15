4989589b5f6d64aae3f9122cce240c63cd403c75
nhoogenkamp <152394888+nhoogenkamp@users.noreply.github.com>
2026-01-15 20:26:23 +0000

    Updated project

--- task-manager/src/pages/About.jsx
+++ task-manager/src/pages/About.jsx
@@ -11,6 +11,7 @@ export default function About() {
         This app helps you manage tasks with priority, due dates, and a
         responsive interface. Built using React, Material UI, and the
         Context API.
+        This is another test for using GIT
       </Typography>
     </Container>
   );
